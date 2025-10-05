d0ca234ee74cb85847be04b05543c108dde4bf89
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-10-05 08:25:24 +0000

    Update ROUGE

--- rouge.go
+++ rouge.go
@@ -1,17 +1,42 @@
 package reval
 
+// ROUGE1 returns the ROUGE-1 score (unigram overlap)
 func ROUGE1(candidates, refs []string) (precision, recall, f1 float64) {
+	return rouge(candidates, refs, 1.0, Overlap)
+}
+
+// ROUGEL returns the ROUGE-L score (Longest Common Subsequence)
+func ROUGEL(candidates, refs []string) (precision, recall, f1 float64) {
+	return rouge(candidates, refs, 1.0, LCS)
+}
+
+func rouge(candidates, refs []string, beta float64, f func(a, b []string) int) (precision, recall, f1 float64) {
 	if len(candidates) == 0 || len(refs) == 0 {
 		return 0, 0, 0
 	}
 
-	count := make(map[string]int, len(refs))
-	for _, token := range refs {
+	// overlap, LCS, ...
+	matched := f(candidates, refs)
+
+	// score
+	precision = float64(matched) / float64(len(candidates))
+	recall = float64(matched) / float64(len(refs))
+	if precision+recall > 0 {
+		f1 = FBeta(precision, recall, beta)
+	}
+
+	return
+}
+
+// Overlap returns the count of overlapping tokens between a and b.
+func Overlap(a, b []string) int {
+	count := make(map[string]int, len(a))
+	for _, token := range a {
 		count[token]++
 	}
 
 	var overlap int
-	for _, token := range candidates {
+	for _, token := range b {
 		if count[token] < 1 {
 			continue
 		}
@@ -20,16 +45,35 @@ func ROUGE1(candidates, refs []string) (precision, recall, f1 float64) {
 		count[token]--
 	}
 
-	// score
-	precision = float64(overlap) / float64(len(candidates))
-	recall = float64(overlap) / float64(len(refs))
-	if precision+recall > 0 {
-		f1 = FBeta(precision, recall, 1.0)
-	}
-
-	return
+	return overlap
 }
 
+// LCS returns the length of the Longest Common Subsequence between a and b.
+func LCS(a, b []string) int {
+	m, n := len(a), len(b)
+
+	// dynamic programming table
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	// fill dp table
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if a[i-1] == b[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+				continue
+			}
+
+			dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+		}
+	}
+
+	return dp[m][n]
+}
+
+// FBeta returns the F-beta score given precision and recall.
 func FBeta(precision, recall, beta float64) float64 {
 	if precision == 0 && recall == 0 {
 		return 0
--- rouge_test.go
+++ rouge_test.go
@@ -18,6 +18,327 @@ func ExampleROUGE1() {
 	// 0.7143, 0.8333, 0.7692
 }
 
+func ExampleROUGEL() {
+	candidates := []string{"the", "cat", "is", "sitting", "on", "the", "mat"}
+	refs := []string{"the", "cat", "sat", "on", "the", "mat"}
+
+	precision, recall, f1 := reval.ROUGEL(candidates, refs)
+	fmt.Printf("%.4f, %.4f, %.4f\n", precision, recall, f1)
+
+	// Output:
+	// 0.7143, 0.8333, 0.7692
+}
+
+func TestROUGE1(t *testing.T) {
+	cases := []struct {
+		candidates []string
+		refs       []string
+		precision  float64
+		recall     float64
+		f1         float64
+	}{
+		{
+			candidates: []string{"a", "b", "c"},
+			refs:       []string{"a", "b", "c"},
+			precision:  1.0,
+			recall:     1.0,
+			f1:         1.0,
+		},
+		{
+			candidates: []string{"a", "b", "c"},
+			refs:       []string{"b", "c", "d"},
+			precision:  2.0 / 3.0,
+			recall:     2.0 / 3.0,
+			f1:         2.0 / 3.0,
+		},
+		{
+			candidates: []string{"a", "b", "c"},
+			refs:       []string{"c", "b", "a"},
+			precision:  1.0,
+			recall:     1.0,
+			f1:         1.0,
+		},
+		{
+			candidates: []string{"a", "b", "a", "c"},
+			refs:       []string{"a", "a", "c"},
+			precision:  3.0 / 4.0,
+			recall:     1.0,
+			f1:         0.8571428571428571,
+		},
+		{
+			candidates: []string{"x", "y", "z"},
+			refs:       []string{"y", "z"},
+			precision:  2.0 / 3.0,
+			recall:     1.0,
+			f1:         0.8,
+		},
+		{
+			candidates: []string{},
+			refs:       []string{"a"},
+			precision:  0.0,
+			recall:     0.0,
+			f1:         0.0,
+		},
+		{
+			candidates: []string{"a"},
+			refs:       []string{},
+			precision:  0.0,
+			recall:     0.0,
+			f1:         0.0,
+		},
+		{
+			candidates: []string{"a", "a", "b", "b"},
+			refs:       []string{"a", "b", "b", "b"},
+			precision:  3.0 / 4.0,
+			recall:     3.0 / 4.0,
+			f1:         0.75,
+		},
+		{
+			candidates: []string{"a", "b", "c", "a"},
+			refs:       []string{"a", "c", "a", "b"},
+			precision:  1.0,
+			recall:     1.0,
+			f1:         1.0,
+		},
+	}
+
+	for _, c := range cases {
+		precision, recall, f1 := reval.ROUGE1(c.candidates, c.refs)
+		if !reval.IsClose(precision, c.precision) {
+			t.Errorf("got=%v, want=%v", precision, c.precision)
+		}
+
+		if !reval.IsClose(recall, c.recall) {
+			t.Errorf("got=%v, want=%v", recall, c.recall)
+		}
+
+		if !reval.IsClose(f1, c.f1) {
+			t.Errorf("got=%v, want=%v", f1, c.f1)
+		}
+	}
+}
+
+func TestROUGEL(t *testing.T) {
+	cases := []struct {
+		candidates []string
+		refs       []string
+		precision  float64
+		recall     float64
+		f1         float64
+	}{
+		{
+			candidates: []string{},
+			refs:       []string{},
+			precision:  0.0,
+			recall:     0.0,
+			f1:         0.0,
+		},
+		{
+			candidates: []string{"a", "b", "c"},
+			refs:       []string{"a", "b", "c"},
+			precision:  1.0,
+			recall:     1.0,
+			f1:         1.0,
+		},
+		{
+			candidates: []string{"a", "b", "c"},
+			refs:       []string{"b", "c", "d"},
+			precision:  2.0 / 3.0,
+			recall:     2.0 / 3.0,
+			f1:         2.0 / 3.0,
+		},
+		{
+			candidates: []string{"a", "b", "c"},
+			refs:       []string{"c", "b", "a"},
+			precision:  1.0 / 3.0,
+			recall:     1.0 / 3.0,
+			f1:         1.0 / 3.0,
+		},
+		{
+			candidates: []string{"a", "b", "a", "c"},
+			refs:       []string{"a", "a", "c"},
+			precision:  3.0 / 4.0,
+			recall:     1.0,
+			f1:         0.8571428571428571,
+		},
+		{
+			candidates: []string{"x", "y", "z"},
+			refs:       []string{"y", "z"},
+			precision:  2.0 / 3.0,
+			recall:     1.0,
+			f1:         0.8,
+		},
+		{
+			candidates: []string{"a"},
+			refs:       []string{"a"},
+			precision:  1.0,
+			recall:     1.0,
+			f1:         1.0,
+		},
+		{
+			candidates: []string{"a"},
+			refs:       []string{"b"},
+			precision:  0.0,
+			recall:     0.0,
+			f1:         0.0,
+		},
+		{
+			candidates: []string{"a", "b", "c", "a"},
+			refs:       []string{"a", "c", "a", "b"},
+			precision:  3.0 / 4.0,
+			recall:     3.0 / 4.0,
+			f1:         0.75,
+		},
+	}
+
+	for _, c := range cases {
+		precision, recall, f1 := reval.ROUGEL(c.candidates, c.refs)
+		if !reval.IsClose(precision, c.precision) {
+			t.Errorf("got=%v, want=%v", precision, c.precision)
+		}
+
+		if !reval.IsClose(recall, c.recall) {
+			t.Errorf("got=%v, want=%v", recall, c.recall)
+		}
+
+		if !reval.IsClose(f1, c.f1) {
+			t.Errorf("got=%v, want=%v", f1, c.f1)
+		}
+	}
+}
+
+func TestOverlap(t *testing.T) {
+	cases := []struct {
+		a    []string
+		b    []string
+		want int
+	}{
+		{
+			a:    []string{},
+			b:    []string{},
+			want: 0,
+		},
+		{
+			a:    []string{"a", "b"},
+			b:    []string{},
+			want: 0,
+		},
+		{
+			a:    []string{},
+			b:    []string{"a", "b"},
+			want: 0,
+		},
+		{
+			a:    []string{"a", "b", "c"},
+			b:    []string{"a", "b", "c"},
+			want: 3,
+		},
+		{
+			a:    []string{"a", "b", "c"},
+			b:    []string{"b", "c", "d"},
+			want: 2,
+		},
+		{
+			a:    []string{"a", "b", "c"},
+			b:    []string{"c", "b", "a"},
+			want: 3,
+		},
+		{
+			a:    []string{"a", "b", "a", "c"},
+			b:    []string{"a", "a", "c"},
+			want: 3,
+		},
+		{
+			a:    []string{"x", "y", "z"},
+			b:    []string{"y", "z"},
+			want: 2,
+		},
+		{
+			a:    []string{"a"},
+			b:    []string{"a"},
+			want: 1,
+		},
+		{
+			a:    []string{"a"},
+			b:    []string{"b"},
+			want: 0,
+		},
+	}
+
+	for _, c := range cases {
+		got := reval.Overlap(c.a, c.b)
+		if got != c.want {
+			t.Errorf("got=%v, want=%v", got, c.want)
+		}
+	}
+}
+
+func TestLCS(t *testing.T) {
+	cases := []struct {
+		a    []string
+		b    []string
+		want int
+	}{
+		{
+			a:    []string{},
+			b:    []string{},
+			want: 0,
+		},
+		{
+			a:    []string{"a", "b"},
+			b:    []string{},
+			want: 0,
+		},
+		{
+			a:    []string{},
+			b:    []string{"a", "b"},
+			want: 0,
+		},
+		{
+			a:    []string{"a", "b", "c"},
+			b:    []string{"a", "b", "c"},
+			want: 3,
+		},
+		{
+			a:    []string{"a", "b", "c"},
+			b:    []string{"b", "c", "d"},
+			want: 2,
+		},
+		{
+			a:    []string{"a", "b", "c"},
+			b:    []string{"c", "b", "a"},
+			want: 1,
+		},
+		{
+			a:    []string{"a", "b", "a", "c"},
+			b:    []string{"a", "a", "c"},
+			want: 3,
+		},
+		{
+			a:    []string{"x", "y", "z"},
+			b:    []string{"y", "z"},
+			want: 2,
+		},
+		{
+			a:    []string{"a"},
+			b:    []string{"a"},
+			want: 1,
+		},
+		{
+			a:    []string{"a"},
+			b:    []string{"b"},
+			want: 0,
+		},
+	}
+
+	for _, c := range cases {
+		got := reval.LCS(c.a, c.b)
+		if got != c.want {
+			t.Errorf("got=%v, want=%v", got, c.want)
+		}
+	}
+}
+
 func TestFBeta(t *testing.T) {
 	cases := []struct {
 		precision float64
